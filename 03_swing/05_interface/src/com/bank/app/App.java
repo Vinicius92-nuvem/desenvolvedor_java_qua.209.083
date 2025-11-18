0ed05e825e7ecf52b872bd7b9a584d1aa00f7765
Vinicius92-nuvem <viniciusgsilva92@gmail.com>
2025-11-17 21:37:21 -0300

    Aula do dia 17/11/2025.

--- 03_swing/05_interface/src/com/bank/app/App.java
+++ 03_swing/05_interface/src/com/bank/app/App.java
@@ -6,16 +6,63 @@ import com.bank.models.Conta;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       Conta cc = new Conta(null, null, "1234-5", "12345-6", 0.0);
+        Conta cc = new Conta(null, null, "1234-5", "12345-6", 0.0);
 
-       String[] opcoes = {"Consultar dados ", "Depositar", "Sacar", "Sair" };
+        String[] opcoes = {
+            "Consultar dados", 
+            "Depositar", 
+            "Sacar", 
+            "Sair"
+        };
+        Object opcao;
 
-       Object opcao;
+        double valor;
 
-       cc.setTitular(JOptionPane.showInputDialog("Informe o nome do titular da conta:"));
-       cc.setCpf(JOptionPane.showInputDialog("Informe o CPF do titular da conta:"));
+        cc.setTitular(JOptionPane.showInputDialog("Informe o nome do titular da conta:"));
+        cc.setCpf(JOptionPane.showInputDialog("Informe o CPF do titular da conta:"));
 
-    //    TODO: fazer o laço de repetição...
-
+        do {
+            opcao = JOptionPane.showInputDialog(
+                null, 
+                "Escolha a opção desejada", 
+                null, 
+                JOptionPane.QUESTION_MESSAGE, 
+                null, 
+                opcoes, 
+                opcoes[0]
+            );
+            
+            if (opcao == "Consultar dados") {
+                JOptionPane.showMessageDialog(null, cc.consultarDados());
+            }
+            else if (opcao == "Depositar") {
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor do depósito em R$: ").replace(",", "."));
+                JOptionPane.showMessageDialog(
+                    null, 
+                    "Depósito efetuado com sucesso." + 
+                    "\nValor do saldo: R$ " + 
+                    String.format("%.2f", cc.depositar(valor))
+                );
+            }
+            else if (opcao == "Sacar") {
+                valor = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor do saque em R$: ").replace(",", "."));
+                if (cc.getSaldo() >= valor) {
+                    JOptionPane.showMessageDialog(
+                        null, 
+                        "Saque efetuado com sucesso." + 
+                        "\nValor do saldo: R$ " +
+                        String.format("%.2f", cc.sacar(valor))
+                    );
+                }
+                else {
+                    JOptionPane.showMessageDialog(
+                        null, 
+                        "Saldo insuficiente." +
+                        "\nSaldo atual: R$ " +
+                        String.format("%.2f", cc.getSaldo())
+                    );
+                }
+            }
+        } while (opcao != "Sair");
     }
 }
